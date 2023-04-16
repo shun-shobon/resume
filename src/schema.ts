fb1818a5b31c7b3fe149b2bc55ab83b6ad691e7a
Shuntaro Nishizawa <me@s2n.tech>
2023-04-17 02:43:17 +0900

    feat: Add types

--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
+export type Link = z.infer<typeof Link>;
 export const Link = z.object({
   name: z.string(),
   url: z.string(),
 });
 
+export type Date = z.infer<typeof Date>;
 export const Date = z.union([
   z.string(),
   z.object({
@@ -13,6 +15,7 @@ export const Date = z.union([
   }),
 ]);
 
+export type Basic = z.infer<typeof Basic>;
 export const Basic = z.object({
   name: z.string(),
   aka: z.string(),
@@ -20,6 +23,7 @@ export const Basic = z.object({
   interested: z.array(z.string()),
 });
 
+export type History = z.infer<typeof History>;
 export const History = z.object({
   date: Date,
   title: z.string(),
@@ -27,6 +31,7 @@ export const History = z.object({
   description: z.string().optional(),
 });
 
+export type Event = z.infer<typeof Event>;
 export const Event = z.object({
   date: z.string(),
   title: z.string(),
@@ -35,6 +40,7 @@ export const Event = z.object({
   links: z.array(Link).optional(),
 });
 
+export type Works = z.infer<typeof Works>;
 export const Works = z.object({
   title: z.string(),
   description: z.string(),
@@ -42,11 +48,13 @@ export const Works = z.object({
   links: z.array(Link).optional(),
 });
 
+export type Skill = z.infer<typeof Skill>;
 export const Skill = z.object({
   name: z.string(),
   level: z.number(),
 });
 
+export type Resume = z.infer<typeof Resume>;
 export const Resume = z.object({
   basic: Basic,
   history: z.array(History),
