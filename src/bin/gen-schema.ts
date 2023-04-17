eb41cace1a43b75d5956eb00c05d67b731cead24
Shuntaro Nishizawa <me@s2n.tech>
2023-04-17 15:51:54 +0900

    chore: Use node.js instead of deno

--- src/bin/gen-schema.ts
+++ src/bin/gen-schema.ts
@@ -1,5 +1,8 @@
+import * as fs from "node:fs/promises";
+
 import { zodToJsonSchema } from "zod-to-json-schema";
-import { Resume } from "../schema.ts";
+
+import { Resume } from "../schema.js";
 
 const schema = zodToJsonSchema(Resume, "resume");
-Deno.writeTextFile("schema.json", JSON.stringify(schema, null, 2));
+await fs.writeFile("schema.json", JSON.stringify(schema, null, 2));
